215a8c49c65a6e6547393354640ae2a9648fdc09
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2019-11-25 09:54:26 +0100

    H3 Af

---
+++ src/H3/H3Op1.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<?php
+for($i = 0; $i <= 9; $i++){
+   for($j = 0; $j < 9; $j++) {
+   echo "*";
+   }
+   echo "*<br>";
+}
+?>
+</body>
+</html>
